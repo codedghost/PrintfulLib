049b0e665a6bb5991afa2902f65c28d5a62cbf61
codedghost2 <codedghost2@gmail.com>
2020-01-13 14:21:49 +0000

    Making Printful library a little more generic for general use

---
+++ CoreCodedChatbot.Printful/TestHarness/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CoreCodedChatbot.Printful.ExternalClients;
+
+namespace TestHarness
+{
+    /// <summary>
+    /// This is a test harness used to demonstrate Printful Client usage and to test usage
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // It is recommended to wrap the Client creation in a Factory class for DI purposes
+            var client = new PrintfulClient("INSERT-API-KEY");
+
+            var keepRunning = true;
+
+            while (keepRunning)
+            {
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                }
+            }
+        }
+    }
+}
