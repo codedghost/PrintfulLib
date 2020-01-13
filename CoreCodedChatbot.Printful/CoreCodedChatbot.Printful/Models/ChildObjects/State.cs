e7d3fd14fcf99041ab5baa3126573411cab63d2c
codedghost2 <codedghost2@gmail.com>
2020-01-13 15:49:54 +0000

    Adding Country code query models

---
+++ CoreCodedChatbot.Printful/CoreCodedChatbot.Printful/Models/ChildObjects/State.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace PrintfulLib.Models.ChildObjects
+{
+    public class State
+    {
+        /// <summary>
+        /// State Code - Mandatory when creating an order for USA, Canada and Australia
+        /// </summary>
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// State name
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
