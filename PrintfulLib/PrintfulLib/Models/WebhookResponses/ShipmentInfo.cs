819523300ab338428cc1a3369a29c571b64be80d
codedghost2 <codedghost2@gmail.com>
2021-02-03 19:38:57 +0000

    Creating webhook data response classes

---
+++ PrintfulLib/PrintfulLib/Models/WebhookResponses/ShipmentInfo.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using PrintfulLib.Models.ChildObjects;
+
+namespace PrintfulLib.Models.WebhookResponses
+{
+    public class ShipmentInfo : IWebhookDataObject
+    {
+        [JsonProperty("shipment")]
+        public Shipment Shipment { get; set; }
+
+        [JsonProperty("order")]
+        public Order Order { get; set; }
+    }
+}
---
+++ PrintfulLib/PrintfulLib/Models/WebhookResponses/VariantStock.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace PrintfulLib.Models.WebhookResponses
+{
+    public class VariantStock
+    {
+        [JsonProperty("out")]
+        public int[] VariantsOutOfStock { get; set; }
+
+        [JsonProperty("discontinued")]
+        public int[] VariantsDiscontinued { get; set; }
+    }
+}
